bef6abb7c85b025e73d03dd84dd999e8051a44f6
it-school <GITHUB it@osvita.od.ua>
2021-09-02 12:23:45 +0300

    Initial commit

---
+++ src/com/itschool/Main.java
@@ -0,0 +1,11 @@
+package com.itschool;
+
+public class Main {
+
+   public static void main(String[] args) {
+      System.out.println("thdyujfikohjkl");
+      for (int k = 0; k < 2; k++) {
+         System.out.println("fndhgmfjhgk,jlhkj");
+      }
+   }
+}
