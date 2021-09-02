fc66a483cc10f6ae299e7f08d4a70aeeef3f95b1
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2021-09-02 12:29:17 +0300

    Update Main.java
    Numbers added

--- src/com/itschool/Main.java
+++ src/com/itschool/Main.java
@@ -5,8 +5,8 @@ public class Main {
    public static void main(String[] args) {
       System.out.println("thdyujfikohjkl");
       for (int k = 0; k < 2; k++) {
-         System.out.println("fndhgmfjhgk,jlhkj");
+         System.out.println("1234567,jlhkj");
       }
-      System.out.println("rhtydjtuyfkiulgoihpm");
+      System.out.println("9876543");
    }
 }
